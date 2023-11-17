fb1f5ef55d88e70304a1eb608c66b21de71d0d39
takeru0076 <takeru200276@icloud.com>
2023-11-17 17:27:58 +0900

    modelとmigrationの使い方

--- django_first/diary/models.py
+++ django_first/diary/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Diary(models.Model):
+
+
+    class Meta:
+        db_table = 'diary'
+
+
+    title = models.CharField(max_length=100) # タイトル
+    content = models.TextField(max_length=1000) # 内容
+    create_at = models.DateField(auto_now_add=True) # 作成日時
+
+
+    def __str__(self):
+        return self.title
